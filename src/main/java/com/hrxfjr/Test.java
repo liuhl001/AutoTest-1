345cd6703b57042a2997b15abb0893491a4d4fab
unknown <zhangxiaochu@chamc.com.cn>
2018-11-02 15:27:57 +0800

    Initial commit

---
+++ src/main/java/com/hrxfjr/Test.java
@@ -0,0 +1,13 @@
+package com.hrxfjr;
+
+/**
+ * @Auther: RCY
+ * @Date: 2018/11/2 15:17
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("124124");
+    }
+
+}
